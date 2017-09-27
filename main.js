a1e59016d7dced67a8add1db75431c20feb8c081
scottmacaz <scottmacaz@yahoo.com>
2017-09-26 18:07:40 -0700

    updated main.js with open file dialog

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const {app, BrowserWindow, ipcMain} = require('electron')
+const {app, BrowserWindow, ipcMain, dialog} = require('electron')
+var fs = require('fs');
 let win = null;
 function createWindow() {
   // Initialize the window to our specified dimensions
@@ -10,6 +11,7 @@ function createWindow() {
   win.webContents.openDevTools()
   // Remove window once app is closed
   win.on('closed', function () {
+    console.log("closed ....");
     win = null;
   });
 }
@@ -24,11 +26,46 @@ app.on('ready', function () {
   })
   app.on('window-all-closed', function () {
     if (process.platform != 'darwin') {
+      console.log("Exiting ....");
       app.quit();
     }
   });
 
   ipcMain.on('open-file', (event, arg) => {  
+
+    dialog.showOpenDialog({ properties: ['openFile'], filters: [{ name: 'CSV', extensions: ['csv'] }, { name: 'ALL', extensions: ['*'] }] },
+    function (fileNames) {
+      if (fileNames == undefined) return;
+      var fileName = fileNames[0];
+
+
+      fs.readFile(fileName, 'utf-8', function (err, data) {
+        if (err != null) {
+
+          console.log("Error!" + err)
+          dialog.showErrorBox("File Read Error", err.message);
+          return;
+        }
+
+        console.log(fileName);
+        console.log(data);
+        dialog.showMessageBox({
+          message: "The file has been read!",
+          buttons: ["OK"]
+        });
+
+        //focusedWindow.webContents.send('file-data', { fileLines: data });
+        event.returnValue = data;
+
+        //document.getElementById("editor").value = data;
+
+      });
+    });
+
+
+
+
+
     // Send value synchronously back to renderer process
-    event.returnValue =  'these are the file contents .....';
+    //event.returnValue =  'these are the file contents .....';
 });
